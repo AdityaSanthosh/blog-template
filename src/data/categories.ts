1a4b7612b65fa545bd0dffd657d426b37fddb769
aditya-palepu <asanthosh@g2.com>
2025-01-01 13:36:18 +0530

    wip 2

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,7 @@
 // List of categories for blog posts
-export const CATEGORIES = ['Programming', 'About Me'] as const
+export const CATEGORIES = [
+	'Programming',
+	'Tech and Tinkerings',
+	'Curious Inklings',
+	'About Me'
+] as const
